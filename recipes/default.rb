f1145ce471accf2309644c7d2a7f3aab4d0cc58b
dev1 <tio.bagio@gmail.com>
2019-09-03 07:31:01 +0800

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: haproxy
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'haproxy'
+
+allwebservers = search('node', 'name:my_web*')
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+  variables(
+    :webservers => allwebservers
+  )
+ notifies :restart, 'service[haproxy]'
+end
+
+service 'haproxy' do
+ action [:start, :enable] 
+end
